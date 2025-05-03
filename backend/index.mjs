868062ef46824941a6d770c4d3f13c0c0d8d2122
zannza <git@github.com.sslab.ics.keio.ac.jp>
2025-05-04 03:03:56 +0900

    addd index.mjs

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,18 +1,54 @@
-// const base64 = require('base-64');  // base64 エンコード用のライブラリをインポート
-import base64 from "base-64"
+import { Buffer } from 'buffer';
+import Pokemon from './src/pokemon.js';
+import BattleField from './src/battleField.js'
+import Characteristic from './src/characteristic.js'; 
+import Items from './src/item.js'; 
+import Move from './src/move.js'
 
 export const handler = async (event) => {
-    // イベントにisBase64Encodedが含まれていない場合
-    if (!event.isBase64Encoded) {
-        console.log(event.body);
-    } else {
-        // イベントにisBase64Encodedが含まれている場合
-        console.log(base64.decode(event.body));
-    }
+    try {
+        // イベントボディがBase64エンコードされているかどうかを判定
+        const requestBody = event.isBase64Encoded
+            ? Buffer.from(event.body, 'base64').toString('utf-8')
+            : event.body;
 
-    // レスポンスを返す
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Request processed successfully' }),
-    };
+        // console.log(requestBody);  // 受け取ったリクエストの内容をログに出力
+        const attacker = new Pokemon(requestBody["attacker"]);
+        const move = new Move(requestBody["move"]);
+        const defender = new Pokemon(requestBody["defender"]);
+        // weather = requestBody["weather"];
+        // field = requestBody["field"];
+        // isMultipleOpponents = requestBody["isMultipleOpponents"];
+        other = requestBody["other"];
+        const battleField = new BattleField({attacker: attacker,
+                                            move: move,
+                                            defender: defender,
+                                            // weather: weather,
+                                            // field: field,
+                                            // isMultipleOpponents, isMultipleOpponents
+                                            weather: 1,
+                                            field: 1,
+                                            isMultipleOpponents: 1
+                                            });
+        battleField.attacker.move = move;
+        damage = battleField.calculateDamage;
+
+        // 正常に処理できた場合のレスポンス
+        return {
+            statusCode: 200,
+            // body: JSON.stringify({ message: 'Request processed successfully' }),
+            body: JSON.stringify({damagerange: damage})
+        };
+    } catch (error) {
+        // エラーハンドリング
+        console.error('Error occurred:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal Server Error',
+                error: error.message,
+            }),
+        };
+    }
 };
--- backend/src/pokemon.js
+++ backend/src/pokemon.js
@@ -2,28 +2,30 @@ class Pokemon {
     #name;
     #ability_list;
     #ability;
+    #characteristic;
     #level;
     #baseStats;
     #ivs;
     #evs;
     #status;
     #nature;
-    #rankModifiers;
+    #rankModifier;
     #move;
     #item;
     #types;
     #weight;
     #terastalType;
-    constructor({name, ability, baseStats, level, ivs, evs, nature, rankModifiers, item, types, weight = null, terastalType = null}) {
+    constructor({name, ability, baseStats, characteristic, level, ivs, evs, nature, rankModifier, item, types, weight = null, terastalType = null}) {
         this.#name = name; // 名前
         this.#ability_list = ability; // 特性
         this.#level = level; // レベル
         this.#baseStats = baseStats; // 種族値
+        this.#characteristic = characteristic; // 性格
         this.#ivs = ivs; // 個体値
         this.#evs = evs; // 努力値
         this.#status = null; // 実数値
         this.#nature = nature; // 性格
-        this.#rankModifiers = rankModifiers; // ランク補正
+        this.#rankModifier = rankModifier; // ランク補正
         this.#move = undefined; // わざ
         this.#item = item; // もちもの
         this.#types = types; // タイプ
@@ -34,11 +36,12 @@ class Pokemon {
     // get property
     get name() { return this.#name; }
     get ability() { return this.#ability_list; }
+    get characteristic() { return this.#characteristic; }
     get level() { return this.#level; }
     get ivs() { return this.#ivs; }
     get evs() { return this.#evs; }
     get nature() { return this.#nature; }
-    get rankModifiers() { return this.#rankModifiers; }
+    get rankModifier() { return this.#rankModifier; }
     get move() { return this.#move; }
     get item() { return this.#item; }
     get types() { return this.#types; }
@@ -47,7 +50,10 @@ class Pokemon {
 
     // set property
     set ability(ability) { this.#ability = ability; }
+    set rankModifier(rankModifier) { this.#rankModifier = rankModifier; }
     set move(move) { this.#move = move; }
+    set item(item) { this.#item = item; }
+    set terastalType(terastalType) { this.#terastalType = terastalType; }
 
 
     // Example method to calculate a stat
